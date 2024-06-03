989c72ee8f105406085788bfb2ddadbe685e9da5
Arnault DOUYERE <arnault.douyere@sii.fr>
2024-06-03 16:09:21 +0200

    p2c5

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,6 +1,33 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
+function handleClick(careType,rangeElem,event){
+	var type;
+	switch(careType){
+		case 'light':
+			type = ' de lumière';
+			break;
+		case 'water':
+			type = ' d\'eau';
+			break;
+	}
+	var qt;
+	switch(rangeElem){
+		case 1:
+			qt = 'peu';
+			break;
+		case 2:
+			qt = 'modérement';
+			break;
+		case 3:
+			qt = 'beaucoup de lumière';
+			break;
+	}
+	alert('Cette plante requiert '+qt+ type)
+	event.stopPropagation();
+	
+}
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
 	const scaleType =
@@ -14,7 +41,7 @@ function CareScale({ scaleValue, careType }) {
 		<div>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
-					<span key={rangeElem.toString()}>{scaleType}</span>
+					<span key={rangeElem.toString()} onClick={(event) => handleClick(careType,rangeElem,event)}>{scaleType}</span>
 				) : null
 			)}
 		</div>
